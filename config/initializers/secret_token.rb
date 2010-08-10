4cb3864904995cce795c05352d9f63ca3024f0a0
Michael He <hlxwell@gmail.com>
2010-08-10 10:36:37 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobware::Application.config.secret_token = 'af91883a17f0b64b2cf690a6f3c918cef879e0579b36e9e2f23b25783e92d5a215eac3ae515346d832730617fd5d9f75a68fe2d106d79aec15cd3cdbe35dd7c3'
