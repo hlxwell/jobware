fd6c13ce8006cddea31e02409dde1c55a4408dbc
Michael He <hlxwell@gmail.com>
2010-08-11 15:15:30 +0800

    add nav plugin

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.0.0.rc'
 
 gem 'mysql'
 
+gem 'navigasmic'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
+require 'pp'
 module ApplicationHelper
+  def in_section?(name)
+    case name
+    when "jobseeker"
+      current_page?('/home/a') or
+      current_page?('/home/b')
+    when "company"
+      current_page?('/home/c') or
+      current_page?('/home/d')
+    when "partner"
+      current_page?('/home/e') or
+      current_page?('/home/f')
+    when "frontend"
+      !('g'..'z').to_a.map {|w| current_page?("/home/#{w}") }.include?(true)
+    else
+      false
+    end
+  end
 end
