f156f5ac76c5087f58576061d2e5ed03c2758a77
Michael He <hlxwell@gmail.com>
2010-09-24 19:33:27 +0800

    add mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,36 @@
 class UserMailer < ActionMailer::Base
   default :from => "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password.subject
+  #
+  def reset_password
+    @greeting = "Hi"
+
+    mail :to => "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.email_confirmation.subject
+  #
+  def email_confirmation
+    @greeting = "Hi"
+
+    mail :to => "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome
+    @greeting = "Hi"
+
+    mail :to => "to@example.org"
+  end
 end
--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # replace this with your real tests
-  test "the truth" do
-    assert true
+  test "reset_password" do
+    mail = UserMailer.reset_password
+    assert_equal "Reset password", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
   end
+
+  test "email_confirmation" do
+    mail = UserMailer.email_confirmation
+    assert_equal "Email confirmation", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "welcome" do
+    mail = UserMailer.welcome
+    assert_equal "Welcome", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
 end
