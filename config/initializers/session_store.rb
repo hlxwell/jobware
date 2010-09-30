44fd12d0b1de95eaf7500274fc8d62f587d899c8
Michael He <hlxwell@gmail.com>
2010-09-30 17:39:44 +0800

    fix session bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-domain = (::Rails.env == 'production' ? ".itjob.fm" : '.lvh.me')
+domain = (::Rails.env == 'production' ? "itjob.fm" : 'lvh.me')
 Jobware::Application.config.session_store :cookie_store, :key => '_jobware_session', :domain => domain
 
 # Use the database for sessions instead of the cookie-based default,
