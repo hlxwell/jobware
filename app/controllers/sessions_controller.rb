737250951949b41862f48f9317bbd549ecef88b0
Michael He <hlxwell@gmail.com>
2010-10-09 18:18:01 +0800

    remove redirect back code for email confirmation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -53,7 +53,7 @@ class SessionsController < ApplicationController
     if @user.is_a?(User)
       flash[:success] = "恭喜您，账户激活成功。您现在可以继续使用了。"
       UserSession.create(@user)
-      redirect_back_or_default(dashboard_path_for(current_user))
+      redirect_to dashboard_path_for(current_user)
     else
       flash[:error] = "对不起您的激活码已经过期了，请重新发送激活码。"
       respond_to do |format|
