636e3cf4b01810fe15b91b98d9d4673285c20ba3
Michael He <hlxwell@gmail.com>
2010-10-10 21:57:58 +0800

    add lazy load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
+  $("img").lazyload({
+    effect : "fadeIn",
+    placeholder : "/images/bg.png"
+  });
+
   $.each($(".city_selector"), function(i, n){ $(n).CitySelector(); });
 
   $(".datepicker").datepicker('option', {changeYear:true, changeMonth:true, yearRange:"1960:2010"});
