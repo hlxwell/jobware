eb6193486a6995aa7c602c5d9aa57b48452fb3bf
Michael He <hlxwell@gmail.com>
2010-10-11 16:28:42 +0800

    move alipay config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
-ActiveMerchant::Billing::Integrations::Alipay::KEY     = "v8eqe23g0lgdre2726vn8f100g041jlx"
-ActiveMerchant::Billing::Integrations::Alipay::ACCOUNT = "2088002016425361"
-ActiveMerchant::Billing::Integrations::Alipay::EMAIL   = "hlxwell@hotmail.com"
-
 class ApplicationController < ActionController::Base
   include Authentication
   protect_from_forgery
