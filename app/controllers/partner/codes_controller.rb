f291eebe2715be61644562df571c12e803e9758b
Michael He <hlxwell@gmail.com>
2010-11-04 18:59:55 +0800

    fix widget bug

--- app/controllers/partner/codes_controller.rb
+++ app/controllers/partner/codes_controller.rb
@@ -1,4 +1,6 @@
 class Partner::CodesController < Partner::BaseController
+  before_filter :find_host
+
   def show
     if current_user.partner.has_partner_site?
       @rss_url = "http://#{current_user.partner.partner_site_style.try(:subdomain)}.itjob.fm/jobs.atom"
@@ -6,8 +8,13 @@ class Partner::CodesController < Partner::BaseController
   end
 
   def update_widget_js_code
+    render :partial => "partner/codes/sidebar_ad_code"
+  end
+
+  private
+
+  def find_host
     subdomain = current_user.try(:partner).try(:partner_site_style).subdomain
     @host = subdomain.blank? ? "itjob.fm" : "#{subdomain}.itjob.fm"
-    render :partial => "partner/codes/sidebar_ad_code"
   end
 end
