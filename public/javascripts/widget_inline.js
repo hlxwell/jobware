68b130cc2c0fbabddb70ccd9bfd7d3fe1d77490b
Michael He <hlxwell@gmail.com>
2010-11-09 11:45:36 +0800

    fix widget editor

--- public/javascripts/widget_inline.js
+++ public/javascripts/widget_inline.js
@@ -2,27 +2,20 @@ if(document.createElement && document.getElementById && document.createTextNode 
   JobletWidget.size.width  = Math.max(JobletWidget.size.width, 600);
   JobletWidget.size.height = Math.max(JobletWidget.size.height, 72);
   JobletWidget.type        = JobletWidget.type || 'flashWidget';
-  for (var i in JobletWidget.color) { JobletWidget.color[i] = JobletWidget.color[i].replace(/^(#)?(\w)(\w)(\w)$/,"$1$2$2$3$3$4$4"); }
+
   var isIE = !+"\v1";
   // Widget Modes
   with (JobletWidget) {
     var Widgets = {
       flashWidget : function () {
-        var htmlWidgetColor = {};
-        for (var i in JobletWidget.color) {
-          htmlWidgetColor[i] = JobletWidget.color[i].replace(/^(#)/,"");
-        }
-
-        with(htmlWidgetColor) {
-          var frameUrl  = joblet_url + '/inline_widget?content=' + content + '&content_bg=' + content_bg;
-          var frame = document.createElement('iframe');
-          frame.setAttribute('width' ,size.width);
-          frame.setAttribute('height',size.height);
-          frame.setAttribute('scrolling', 'no');
-          frame.setAttribute('src',frameUrl);
-          frame.style.border = "solid 1px #" + title_bg;
-          return frame;
-        }
+        var frameUrl  = joblet_url + '/inline_widget?content=' + content;
+        var frame = document.createElement('iframe');
+        frame.setAttribute('width' ,size.width);
+        frame.setAttribute('height',size.height);
+        frame.setAttribute('scrolling', 'no');
+        frame.setAttribute('src',frameUrl);
+        frame.style.border = "1px #ccc solid";
+        return frame;
       }
     }
   }
