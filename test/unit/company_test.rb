71355e8c9672b4a8ca3128ad459e202522eba22f
Michael He <hlxwell@gmail.com>
2010-11-22 14:55:08 +0800

    add test

--- test/unit/company_test.rb
+++ test/unit/company_test.rb
@@ -1,7 +1,35 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class CompanyTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Company.new.valid?
+  context "company" do
+    setup do
+      @hash = {
+        "address"=>"上海市",
+        "city"=>"徐",
+        "name"=>"上海巨一科技",
+        "contact_name"=>"张",
+        "size"=>"3",
+        "open_contact"=>"1",
+        "phone_number"=>"6467-7777",
+        "company_type"=>"1",
+        "tag_list"=>"java",
+        "homepage"=>"http://www.justonetech.com",
+        "desc"=>"上海",
+        "province"=>"上海",
+        "industry"=>"1",
+        "logo" => File.open("test/files/file.gif"),
+        "user_attributes"=>{
+          "password_confirmation"=>"123321", 
+          "password"=>"123321", 
+          "email"=>"hlxwell@gmail.com"
+        }
+      }
+    end
+
+    should "be able to save" do
+      company = Company.new @hash
+      assert company.save
+    end
   end
+
 end
