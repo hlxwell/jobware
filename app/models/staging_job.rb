acc1ed59654ea2e2ac6978c64576b41d190ae827
Michael He <hlxwell@gmail.com>
2010-12-31 10:39:52 +0800

    add blank image

--- app/models/staging_job.rb
+++ app/models/staging_job.rb
@@ -23,7 +23,7 @@
 #
 
 class StagingJob < ActiveRecord::Base
-  validates_uniqueness_of :origin_id, :on => :create, :message => "must be unique"
+#  validates_uniqueness_of :origin_id, :on => :create, :message => "must be unique"
   validates_uniqueness_of :page_url, :on => :create, :message => "must be unique"
   
   before_create :trim_bad_html
