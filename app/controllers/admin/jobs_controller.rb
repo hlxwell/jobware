472e6701f896826ee0539d524d5532d8dce2beba
Michael He <hlxwell@gmail.com>
2011-01-11 17:56:07 +0800

    add only job if company is exist.

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,30 +8,55 @@ class Admin::JobsController < Admin::ResourcesController
 
   def import
     @staging_job = StagingJob.find_by_id(params[:id]) || StagingJob.new
+    @company = Company.where(:name => @staging_job.company_name).first
+    @is_company_exist = @company.present?
 
     if request.get?
-      @company = Company.new
-      @company.build_user
-      @company.jobs.build
+      if @is_company_exist
+        @job = @company.jobs.new
+      else
+        @company = Company.new
+        @company.build_user
+        @company.jobs.build
+      end
     elsif request.post?
-      @company = Company.new(params[:company])
-      if @company.save
-        # active user.
-        @company.user.confirm!
-        # charge money.
-        @company.user.charge! 100, :from => '手动添加'
-        # buy post job service.
-        Service.find(8).buy_from!(@company.user)
-        # active the job.
-        @company.jobs.first.want_to_show
-        @company.jobs.first.set_random_views_count
-        @company.set_random_views_count
+      # only add a new job to exist company
+      if @is_company_exist
+        @job = Job.new(params[:job])
+        if @job.save
+          # charge money.
+          @company.user.charge! 100, :from => '手动添加'
+          # buy post job service.
+          Service.find(8).buy_from!(@company.user)
+          # active the job.
+          @job.want_to_show
+          @job.set_random_views_count
+          @staging_job.update_attribute :state, "published" if @staging_job.persisted?
 
-        @staging_job.update_attribute :state, "published" if @staging_job.persisted?
+          flash[:notice] = "工作创建成功。"
+          redirect_to "/admin/users/edit/#{@company.user.id}"
+          return
+        end
+      else # create a company and job
+        @company = Company.new(params[:company])
+        if @company.save
+          # active user.
+          @company.user.confirm!
+          # charge money.
+          @company.user.charge! 100, :from => '手动添加'
+          # buy post job service.
+          Service.find(8).buy_from!(@company.user)
+          # active the job.
+          @company.jobs.first.want_to_show
+          @company.jobs.first.set_random_views_count
+          @company.set_random_views_count
 
-        flash[:notice] = "工作创建成功。"
-        redirect_to "/admin/users/edit/#{@company.user.id}"
-        return
+          @staging_job.update_attribute :state, "published" if @staging_job.persisted?
+
+          flash[:notice] = "公司和岗位创建成功。"
+          redirect_to "/admin/users/edit/#{@company.user.id}"
+          return
+        end
       end
     end
 
@@ -45,32 +70,36 @@ class Admin::JobsController < Admin::ResourcesController
       doc = Nokogiri::XML(file.read)
       doc.xpath("//Content").find_all { |n|
         d = Nokogiri::XML(n.to_s)
-        staging_job = StagingJob.new({
-          :origin_id             => (d.xpath("//ID").children.to_s).html_safe,
-          :name                  => CGI::unescapeHTML(d.xpath("//岗位名称").children.to_s).html_safe,
-          :location              => CGI::unescapeHTML(d.xpath("//工作地点").children.to_s).html_safe,
-          :vacancy               => CGI::unescapeHTML(d.xpath("//岗位人数").children.to_s).html_safe,
-          :salary_range          => CGI::unescapeHTML(d.xpath("//薪资范围").children.to_s).html_safe,
-          :work_year_requirement => CGI::unescapeHTML(d.xpath("//工作年限").children.to_s).html_safe,
-          :degree_requirement    => CGI::unescapeHTML(d.xpath("//学历要求").children.to_s).html_safe,
-          :page_url              => d.xpath("//PageUrl").children.to_s,
-          :desc                  => CGI::unescapeHTML(d.xpath("//职位描述").children.to_s),
-          :company_name          => CGI::unescapeHTML(d.xpath("//公司名字").children.to_s).html_safe,
-          :company_desc          => CGI::unescapeHTML(d.xpath("//公司简介").children.to_s),
-          :contact               => CGI::unescapeHTML(d.xpath("//联系方式").children.to_s),
-          :industry              => CGI::unescapeHTML(d.xpath("//公司行业").children.to_s).html_safe,
-          :company_size          => CGI::unescapeHTML(d.xpath("//企业规模").children.to_s).html_safe,
-          :company_type          => CGI::unescapeHTML(d.xpath("//企业性质").children.to_s).html_safe,
-          :company_homepage      => CGI::unescapeHTML(d.xpath("//企业网站").children.to_s).html_safe,
-          :company_address       => CGI::unescapeHTML(d.xpath("//公司地址").children.to_s).html_safe,
-          :company_phone_number  => CGI::unescapeHTML(d.xpath("//联系电话").children.to_s).html_safe,
-          :company_contact_name  => CGI::unescapeHTML(d.xpath("//联系人").children.to_s).html_safe,
-          :email                 => CGI::unescapeHTML(d.xpath("//邮箱").children.to_s).html_safe,
-          :state                 => '0'
-        })
+        unless staging_job = StagingJob.where(:page_url => d.xpath("//PageUrl").children.to_s).count > 0
+          staging_job = StagingJob.new({
+            :origin_id             => (d.xpath("//ID").children.to_s).html_safe,
+            :name                  => CGI::unescapeHTML(d.xpath("//岗位名称").children.to_s).html_safe,
+            :location              => CGI::unescapeHTML(d.xpath("//工作地点").children.to_s).html_safe,
+            :vacancy               => CGI::unescapeHTML(d.xpath("//岗位人数").children.to_s).html_safe,
+            :salary_range          => CGI::unescapeHTML(d.xpath("//薪资范围").children.to_s).html_safe,
+            :work_year_requirement => CGI::unescapeHTML(d.xpath("//工作年限").children.to_s).html_safe,
+            :degree_requirement    => CGI::unescapeHTML(d.xpath("//学历要求").children.to_s).html_safe,
+            :page_url              => d.xpath("//PageUrl").children.to_s,
+            :desc                  => CGI::unescapeHTML(d.xpath("//职位描述").children.to_s),
+            :company_name          => CGI::unescapeHTML(d.xpath("//公司名字").children.to_s).html_safe,
+            :company_desc          => CGI::unescapeHTML(d.xpath("//公司简介").children.to_s),
+            :contact               => CGI::unescapeHTML(d.xpath("//联系方式").children.to_s),
+            :industry              => CGI::unescapeHTML(d.xpath("//公司行业").children.to_s).html_safe,
+            :company_size          => CGI::unescapeHTML(d.xpath("//企业规模").children.to_s).html_safe,
+            :company_type          => CGI::unescapeHTML(d.xpath("//企业性质").children.to_s).html_safe,
+            :company_homepage      => CGI::unescapeHTML(d.xpath("//企业网站").children.to_s).html_safe,
+            :company_address       => CGI::unescapeHTML(d.xpath("//公司地址").children.to_s).html_safe,
+            :company_phone_number  => CGI::unescapeHTML(d.xpath("//联系电话").children.to_s).html_safe,
+            :company_contact_name  => CGI::unescapeHTML(d.xpath("//联系人").children.to_s).html_safe,
+            :email                 => CGI::unescapeHTML(d.xpath("//邮箱").children.to_s).html_safe,
+            :state                 => '0'
+          })
 
-        if staging_job.save
-          @saved_jobs << staging_job
+          if staging_job.save
+            @saved_jobs << staging_job
+          else
+            @unsaved_jobs << staging_job
+          end
         else
           @unsaved_jobs << staging_job
         end
@@ -119,6 +148,18 @@ class Admin::JobsController < Admin::ResourcesController
 
   protected
 
+  def set_company_desc_editor_html
+    company_desc_html = @staging_job.company_desc.try(:html_safe).to_s.gsub('"','\'').gsub(/\s/, '')
+    ("CKEDITOR.instances.company_desc_editor.setData(\"#{company_desc_html}\");").html_safe
+  end
+  helper_method :set_company_desc_editor_html
+  
+  def set_job_requirement_html
+    job_requirement = @staging_job.desc.try(:html_safe).to_s.gsub('"', '\'').gsub(/\s/, '')
+    ("CKEDITOR.instances.job_requirement.setData(\"#{job_requirement}\");").html_safe
+  end
+  helper_method :set_job_requirement_html
+
   def calculate_company_type(staging_job)
     CompanyType.to_sorted_a.detect do |t|
       staging_job.company_type.try(:html_safe).try(:include?, t.first)
@@ -134,7 +175,7 @@ class Admin::JobsController < Admin::ResourcesController
   helper_method :calculate_industry
 
   def get_contact staging_job
-    "#{CGI::unescapeHTML(staging_job.company_contact_name.try(:html_safe).to_s)} #{CGI::unescapeHTML(staging_job.email.try(:html_safe))}"
+    "#{CGI::unescapeHTML(staging_job.company_contact_name.try(:html_safe).to_s)} #{CGI::unescapeHTML(staging_job.email.try(:html_safe).to_s)}"
   end
   helper_method :get_contact
 end
