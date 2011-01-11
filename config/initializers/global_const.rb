7142b1dec6673bd0924773f303861e1d5924f36b
Michael He <hlxwell@gmail.com>
2011-01-11 23:01:06 +0800

    improve seo

--- config/initializers/global_const.rb
+++ config/initializers/global_const.rb
@@ -1,15 +1,15 @@
 THEMES = {
-  "rails" => "rails",
-  "php" => "php",
-  "java" => "java",
+  "rails" => "ruby on rails",
+  "php" => "php, lamp",
+  "java" => "java, Hibernate, Spring, Struts, iBATIS, JBoss, JSF, Groovy, Lucene",
   "dotnet" => "c#.net vb.net asp.net",
-  "python" => "python",
-  "designer" => "美工设计",
-  "iphone" => "cocoa, ios, iphone",
+  "python" => "python, django",
+  "designer" => "美工设计, html, div, css, javascript, jquery",
+  "iphone" => "cocoa, ios, iphone, objective-c, xcode, 移动开发, mobile",
   "android" => "android",
   "database" => "数据库, mysql, oracle, sql server",
-  "c" => "c, c++, vc",
-  "game" => "游戏开发",
-  "ria" => "flash, flex, silverlight"
+  "c" => "c, c++, vc, 嵌入式开发",
+  "game" => "游戏开发, web游戏, 桌面游戏, 网络游戏",
+  "ria" => "flash, flex, silverlight, ActionScript"
 }
 PRESERVED_SUBDOMAINS = %w{www help} + THEMES.keys
