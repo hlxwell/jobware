c5506fdaa5e85add7b26868182dc49a45350b074
Michael He <hlxwell@gmail.com>
2011-01-19 17:11:21 +0800

    add index for staging jobs for improve the import speed

---
+++ db/migrate/20110119090506_add_index_for_staging_jobs.rb
@@ -0,0 +1,9 @@
+class AddIndexForStagingJobs < ActiveRecord::Migration
+  def self.up
+    add_index :staging_jobs, :page_url
+  end
+
+  def self.down
+    remove_index :staging_jobs, :page_url
+  end
+end
