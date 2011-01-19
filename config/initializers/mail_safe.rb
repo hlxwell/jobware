f52db401fe9fd06a7240ba6fe9538ed4cbad1316
Michael He <hlxwell@gmail.com>
2011-01-19 13:05:35 +0800

    change back the mail safe config

--- config/initializers/mail_safe.rb
+++ config/initializers/mail_safe.rb
@@ -1,7 +1,4 @@
 if defined?(MailSafe::Config) and Rails.env == 'development'
   MailSafe::Config.internal_address_definition = /.*/i
   MailSafe::Config.replacement_address = 'hlxwell@gmail.com'
-else
-  MailSafe::Config.internal_address_definition = /itjob.fm.*@gmail\.com/i
-  MailSafe::Config.replacement_address = 'hlxwell@gmail.com'
 end
