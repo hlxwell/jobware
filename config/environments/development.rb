f423edbd910c44579756e7d3f7b73c7c9f8417a7
Michael He <hlxwell@gmail.com>
2011-03-14 18:20:27 +0800

    updated mail engine and changed the latest config style.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,21 +23,21 @@ Jobware::Application.configure do
   # Send email config
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    :location => '/usr/sbin/sendmail',
-    :arguments => '-i -t'
-  }
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address              => "smtp.sendgrid.net",
-  #   :port                 => '25',
-  #   :domain               => 'itjob.fm',
-  #   :user_name            => 'a@theplant.jp',
-  #   :password             => 'plant^o^',
-  #   :authentication       => :plain
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :port                 => '25',
+    :domain               => 'itjob.fm',
+    :user_name            => 'a@theplant.jp',
+    :password             => 'plant^o^',
+    :authentication       => :plain
+  }
 end
