5e6f6f380f37f767281c4e999e778eef08b42dae
Michael He <hlxwell@gmail.com>
2011-03-18 20:09:16 +0800

    change mail engine config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,13 +50,8 @@ Jobware::Application.configure do
   # Send email config
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :sendmail
-  # config.action_mailer.sendmail_settings = {
-  #   :location => '/usr/sbin/sendmail',
-  #   :arguments => '-i -t'
-  # }
+  config.action_mailer.delivery_method = :sendmail
 
-  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   :address              => "smtp.gmail.com",
   #   :port                 => 587,
@@ -67,14 +62,14 @@ Jobware::Application.configure do
   #   :enable_starttls_auto => true
   # }
 
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.sendgrid.net",
-    :port                 => '25',
-    :domain               => 'itjob.fm',
-    :user_name            => 'a@theplant.jp',
-    :password             => 'plant^o^',
-    :authentication       => :plain
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.sendgrid.net",
+  #   :port                 => '25',
+  #   :domain               => 'itjob.fm',
+  #   :user_name            => 'a@theplant.jp',
+  #   :password             => 'plant^o^',
+  #   :authentication       => :plain
+  # }
 
   config.action_mailer.default_url_options = { :host => "itjob.fm" }
 
