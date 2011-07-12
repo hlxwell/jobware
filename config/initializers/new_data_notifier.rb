dd8e15ac1aee90fbee8040f2b5c0f9494a2e9e92
Michael He <hlxwell@gmail.com>
2011-07-12 10:12:56 +0800

    change new data notification address

--- config/initializers/new_data_notifier.rb
+++ config/initializers/new_data_notifier.rb
@@ -1,3 +1,3 @@
-NewDataNotifier::Notifier.default_recipients = ["hlxwell@qq.com"]
+NewDataNotifier::Notifier.default_recipients = ["hlxwell@gmail.com"]
 NewDataNotifier::Notifier.be_monitored_models = ["Job", "User", "Company", "JobApplication", "Resume"]
 NewDataNotifier::Notifier.sender_address = ["new_data@itjob.fm"]
